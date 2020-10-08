f3b0abdedf404625c6160403751edfd5eedb412d
Awa <francisawa2@gmail.com>
2020-10-07 19:38:37 -0700

    updated code time and storage

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
+    var timeTitle = moment().format("dddd, MMMM Do")
+    $("#currentDay").text(timeTitle)
+
     var arr = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
     function createTimeBlocks() {
-        for (var i =0; i< 8; i++){
+        for (var i = 0; i< 9; i++){
             var timeBlock = $("<div>")
             timeBlock.addClass("row timeBlock")
             timeBlock.attr("id", arr[i])
@@ -41,7 +44,7 @@ $(document).ready(function(){
     }
 
     function timeUpdate(){
-        var currentTime = moment().hours()
+        var currentTime = moment().hours() 
 
         $(".timeBlock").each(function(){
             var blockTime = $(this).attr("id")
@@ -60,6 +63,7 @@ $(document).ready(function(){
         })
     }
     timeUpdate()
+    setInterval(timeUpdate, 60000)
 
     $(".saveBtn").on("click", function(){
         var text = $(this).siblings(".text").val()
@@ -71,8 +75,17 @@ $(document).ready(function(){
     // $(".timeBlock").each(function(){
     //     $(`#${this.attr("id")} .text`).val(JSON.parse(localStorage.getItem(`${this.attr("id")}`)))
     // })
+
     
     $("#9AM .text").val(JSON.parse(localStorage.getItem("9AM")))
+    $("#10AM .text").val(JSON.parse(localStorage.getItem("10AM")))
+    $("#11AM .text").val(JSON.parse(localStorage.getItem("11AM")))
+    $("#12AM .text").val(JSON.parse(localStorage.getItem("12PM")))
+    $("#1AM .text").val(JSON.parse(localStorage.getItem("1PM")))
+    $("#2AM .text").val(JSON.parse(localStorage.getItem("2PM")))
+    $("#3AM .text").val(JSON.parse(localStorage.getItem("3PM")))
+    $("#4AM .text").val(JSON.parse(localStorage.getItem("4PM")))
+    $("#5AM .text").val(JSON.parse(localStorage.getItem("5PM")))
 
 
 })
